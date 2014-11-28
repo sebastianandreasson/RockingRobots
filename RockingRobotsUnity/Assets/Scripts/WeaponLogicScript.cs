20277fd8d2e7f3cef4a89d9ffb6a03fb7ede4a9f
CA Ewerbring <caewerbring@gmail.com>
2014-11-28 21:49:44 +0100

    Added weapon scripts

---
+++ RockingRobotsUnity/Assets/Scripts/WeaponLogicScript.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WeaponLogicScript : MonoBehaviour
+{
+
+		// Use this for initialization
+		void Start ()
+		{
+	
+		}
+	
+		// Update is called once per frame
+		void Update ()
+		{
+	
+				GameObject objectThatWeArePointingAt = objectInWeaponsDirection ();
+	
+		}
+	
+		GameObject objectInWeaponsDirection ()
+		{
+				RaycastHit hit;
+				if (Physics.Raycast (transform.position, transform.parent.transform.forward, out hit)) {
+						float distanceToGround = hit.distance;
+						Vector3 positionOfObjectThatWasHit = hit.collider.gameObject.transform.localPosition;
+				}
+		}
+}
