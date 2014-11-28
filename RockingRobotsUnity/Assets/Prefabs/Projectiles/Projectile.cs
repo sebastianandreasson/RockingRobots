11d047c12d8172414c16249e7ec6ce0768704b79
CA Ewerbring <caewerbring@gmail.com>
2014-11-28 22:27:25 +0100

    Added enemywalking, projectile shader logic

--- RockingRobotsUnity/Assets/Prefabs/Projectiles/Projectile.cs
+++ RockingRobotsUnity/Assets/Prefabs/Projectiles/Projectile.cs
@@ -1,15 +1,43 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Projectile : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
+public class Projectile : MonoBehaviour
+{
+		public Shader glowShader;
+		private Shader defaultShader;
 	
-	}
+		public float timeToGlow;
+		private float timeToEndGlow;
 	
-	// Update is called once per frame
-	void Update () {
+		// Use this for initialization
+		void Start ()
+		{
+				defaultShader = renderer.material.shader;
+		}
 	
-	}
+		// Update is called once per frame
+		void Update ()
+		{
+				if (Time.time >= timeToEndGlow) {
+						revertBackToRegularShader ();
+				}
+		}
+	
+	
+		public void playerIsAimingAtThisObject ()
+		{
+				timeToEndGlow = Time.time + timeToGlow;
+				showGlowShader ();
+		}
+	
+	
+		void showGlowShader ()
+		{
+				renderer.material.shader = glowShader;
+		}
+	
+		void revertBackToRegularShader ()
+		{
+				renderer.material.shader = defaultShader;
+		}
 }
--- RockingRobotsUnity/Assets/Scripts/WeaponLogicScript.cs
+++ RockingRobotsUnity/Assets/Scripts/WeaponLogicScript.cs
@@ -15,17 +15,24 @@ public class WeaponLogicScript : MonoBehaviour
 		{
 				
 				GameObject objectThatWeArePointingAt = objectInWeaponsDirection ();
-	
+				Projectile projectileScript = objectThatWeArePointingAt.GetComponent<Projectile> ();
+				if (projectileScript) {
+						//this was a projectile!
+						//tell it that we pointed to it.
+						projectileScript.playerIsAimingAtThisObject ();
+				}
 		}
 	
 		GameObject objectInWeaponsDirection ()
 		{
 				RaycastHit hit;
+				GameObject gameObjectThatWasHit = null;
 				if (Physics.Raycast (transform.position, transform.parent.transform.forward, out hit)) {
 						float distanceToGround = hit.distance;
 						Vector3 positionOfObjectThatWasHit = hit.collider.gameObject.transform.localPosition;
 						Debug.Log ("found object hit! vector: " + positionOfObjectThatWasHit);
+						gameObjectThatWasHit = hit.collider.gameObject;
 				}
-				return hit.collider.gameObject;
+				return gameObjectThatWasHit;
 		}
 }
