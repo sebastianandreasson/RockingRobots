4548cb98598b66c098a6e327a599c68a03184fe4
CA Ewerbring <caewerbring@gmail.com>
2014-11-28 21:22:27 +0100

    Added greybox projectiles and projectile spawner

---
+++ RockingRobotsUnity/Assets/Projectiles/Spawner/ProjectileSpawner.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ProjectileSpawner : MonoBehaviour
+{
+		public GameObject size1Prefab;
+	
+		// Use this for initialization
+		void Start ()
+		{
+				Vector3 spawnVector = new Vector3 (0, 0, 0);
+				for (int i = 0; i < 10; i ++) {
+						spawnVector = spawnVector + new Vector3 (2, 0, 0);
+						spawnSize1AtVector (spawnVector);
+				}
+		}
+	
+		// Update is called once per frame
+		void Update ()
+		{
+	
+		}
+		
+		
+		void spawnSize1AtVector (Vector3 spawnPoint)
+		{
+				Instantiate (size1Prefab, spawnPoint, Quaternion.identity);
+		}
+}
