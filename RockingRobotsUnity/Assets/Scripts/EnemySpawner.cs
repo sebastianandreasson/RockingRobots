effb34379dcf5fa4eb346448cd077c59a9687184
CA Ewerbring <caewerbring@gmail.com>
2014-11-29 04:45:48 +0100

    Fixed monster spawn!

--- RockingRobotsUnity/Assets/Scripts/EnemySpawner.cs
+++ RockingRobotsUnity/Assets/Scripts/EnemySpawner.cs
@@ -3,58 +3,64 @@ using System.Collections;
 
 public class EnemySpawner : MonoBehaviour
 {
-
+	
 		public GameObject spawnPoint1;
 		public GameObject spawnPoint2;
 		public GameObject spawnPoint3;
-		
+	
 		public GameObject smallEnemy;
 		public GameObject bigEnemy;
-		
+	
 		public float timeBetweenWaves;
 		float timeForNextWave;
 		public float monstersInEachWave;
 		public float timeBetweenMonstersInEachWave;
 		float timeForNextMonsterSpawnInWave;
 		int monstersSpawnedThisWave;
-		
-		
-
+	
+	
+	
 		// Use this for initialization
 		void Start ()
 		{
 				timeForNextWave = Time.time;
 				monstersSpawnedThisWave = 0;
-				
+		
 		}
 	
 		// Update is called once per frame
 		void Update ()
 		{
+				
+				
 				if (Time.time > timeForNextWave && monstersSpawnedThisWave < monstersInEachWave && Time.time > timeForNextMonsterSpawnInWave) {
+						Debug.Log ("Want to spawn monsters");
 						spawnMonstersForWave ();
+						Debug.Log ("next Monster: " + timeForNextMonsterSpawnInWave);
 				} else if (Time.time > timeForNextWave && monstersSpawnedThisWave >= monstersInEachWave) {
 						waveIsFinished ();
 				}
 		}
-		
+	
 		void spawnMonstersForWave ()
 		{
 				spawnMonsterAtPosition (spawnPoint1.transform.position);
 				spawnMonsterAtPosition (spawnPoint2.transform.position);
 				spawnMonsterAtPosition (spawnPoint3.transform.position);
 				monstersSpawnedThisWave += 3;
-				timeForNextMonsterSpawnInWave = Time.time + timeForNextMonsterSpawnInWave;
+				timeForNextMonsterSpawnInWave = Time.time + timeBetweenMonstersInEachWave;
 		}
-		
+	
 		void waveIsFinished ()
 		{
+				Debug.Log ("Wave is finished");
 				monstersSpawnedThisWave = 0;
 				timeForNextWave = Time.time + timeBetweenWaves;
 		}
-		
+	
 		void spawnMonsterAtPosition (Vector3 position)
 		{
+				Debug.Log ("spawning monster!!!!!!!!");
 				Instantiate (smallEnemy, position, Quaternion.identity);
 		
 		}
